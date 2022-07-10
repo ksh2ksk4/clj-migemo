14859d9791e68f4aa2d6dabd5f0e30f2384ad54f
KISHI Kousuke <ksh2ksk4@gmail.com>
2022-07-11 00:11:16 +0900

    wip

--- src/clj_migemo/core.clj
+++ src/clj_migemo/core.clj
@@ -1,9 +1,20 @@
 (ns clj-migemo.core
-  (:require [clj-migemo.dictionaries :as dictionaries])
+  (:require [clj-migemo.dictionaries :as dictionaries]
+            [clojure.core.server :refer :all])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (dictionaries/lookup "wa"))
+  (println
+  (start-server {:address "localhost"
+                 :port 3000
+                 :name "foo"
+                 :accept "clojure.core.server/repl"})
+  )
+  (println *session*)
+  (println (stop-server "foo"))
+  
+  ;(println (dictionaries/lookup "wa"))
+  ;(println (. (java.io.File. ".") getCanonicalPath))
   )
